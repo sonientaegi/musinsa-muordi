3a85943ff7868d0e0318d501e2f77eb55483bbf3
손영택 <sonientaegi@SonienPro.local>
2025-03-28 09:35:18 +0900

    최초 커밋

---
+++ src/main/java/com/musinsa/muordi/MuordiApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.muordi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuordiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuordiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musinsa/muordi/MuordiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.muordi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuordiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
