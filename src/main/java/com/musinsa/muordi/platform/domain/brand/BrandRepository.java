3d389ab032fcd392aad232de8d1c9592e7b16ee5
손영택 <sonientaegi@SonienPro.local>
2025-03-29 16:31:33 +0900

    DTO 배제 - entity 구조 변경. - jpql -> jpa 로 원복.

--- src/main/java/com/musinsa/muordi/platform/domain/brand/BrandRepository.java
+++ src/main/java/com/musinsa/muordi/platform/domain/brand/BrandRepository.java
@@ -13,57 +13,9 @@ import java.util.List;
  */
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
     /**
-     * BrandDTO를 전달받아 DB에 저장한다. id 필드에 값이 있다면 기존 브랜드를 수정하며 null이면 신규레코드를 생성한다.
-     * @param brandDTO 저장 대상 브랜드 DTO
-     * @return 생성 또는 수정한 결과의 DTO
+     * 명칭으로 브랜드를 조회한다.
+     * @param name 조회할 브랜드 명칭
+     * @return 발견한 브랜드 리스트, 또는 빈 리스트
      */
-    default BrandDTO save(BrandDTO brandDTO) {
-        Brand entity = this.save(new Brand(brandDTO.getId(), brandDTO.getName()));
-        return new BrandDTO(entity.getId(), entity.getName());
-    }
-
-    /**
-     * BrandDTO 리스트를 전달받아 DB에 저장한다. id 필드에 값이 있다면 기존 브랜드를 수정하며 null이면 신규레코드를 생성한다.
-     * @param brandDTOList 저장 대상 브랜드 DTO 리스트
-     * @return 생성 또는 수정한 결과의 DTO 리스트
-     */
-    default List<BrandDTO> saveAll(List<BrandDTO> brandDTOList) {
-        return this.saveAll(brandDTOList.stream().map(dto -> new Brand(dto.getId(), dto.getName())).toList())
-                .stream().map(entity -> new BrandDTO(entity.getId(), entity.getName())).toList();
-    }
-
-    /**
-     * 모든 결과를 반환한다.
-     * @return 모든 브랜드 레코드의 DTO를 반환하거나, 미발견시 빈 배열을 반환한다.
-     */
-    @Query(value = """
-        select new com.musinsa.muordi.platform.domain.brand.BrandDTO(brand.id, brand.name)
-            from Brand brand
-    """)
-    List<BrandDTO> all();
-
-    /**
-     * 브랜드를 단건 조회한다.
-     * @param id 조회할 브랜드의 식별자
-     * @return 발견한 브랜드 레코드의 DTO를 반환하거나, null을 반환한다.
-     */
-    @Query(value = """
-        select new com.musinsa.muordi.platform.domain.brand.BrandDTO(brand.id, brand.name)
-            from Brand brand 
-            where brand.id = :id
-    """)
-    BrandDTO byId(@Param("id") Integer id);
-
-    /**
-     * 브랜드 이름으로 브랜드를 조회한다.
-     * @param name 브랜드 이름
-     * @return 발견한 모든 브랜드 레코드의 DTO를 리스트로 반환하거나, 미발견시 빈 리스트를 반환한다.
-     */
-    @Query(value = """
-        select new com.musinsa.muordi.platform.domain.brand.BrandDTO(brand.id, brand.name)
-            from Brand brand 
-            where brand.name = :name
-    """)
-    List<BrandDTO> byName(@Param("name") String name);
-
+    List<Brand> findByName(String name);
 }
